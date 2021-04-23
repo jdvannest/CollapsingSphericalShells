6521e18dc00c23b85ccefa6c8db54a910421792f
jdvannest <jordanvannest@gmail.com>
2021-04-23 15:40:54 -0500

    Updated I/O to print out progress and runtime

--- project.h
+++ project.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <time.h>
 
 void file_name(std::string* fname);
 void count_shells(int* num_shells);
@@ -13,6 +14,7 @@ void initial_energy(const double* mass, const double* r, const double* v, double
 void evolve(double time, double dt, const double* mass, double* r, double* v, double* a, double* energy, const int num_shells);
 void time_params(double* time, double* dt, double* output_time);
 void initial_acc(const double* mass, const double* r, double* a, const int num_shells);
+float round_up(float value);
 double yr_to_s(double years);
 double s_to_yr(double seconds);
 double AU_to_m(double AU);
