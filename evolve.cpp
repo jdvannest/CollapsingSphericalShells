f4653ce7e0f50112b390b16de1fcb4465a717dd8
jdvannest <jordanvannest@gmail.com>
2021-04-29 15:24:15 -0500

    Initial omp parallel loops (probably wrong). Performing energy and acceleration calculations in the same set of loops

--- evolve.cpp
+++ evolve.cpp
@@ -36,6 +36,8 @@ void evolve(double time, double dt, const double* mass, double* r, double* v, do
     float t=0,a_next;
     while(t<time){
         //Calculate new radii for all shells
+        #pragma omp parallel for private(i)
+        {
         for(int i=0;i<num_shells;i+=1){
             r[i] = r[i]+v[i]*dt+0.5*a[i]*pow(dt,2);
             if(r[i]<1){
@@ -44,29 +46,26 @@ void evolve(double time, double dt, const double* mass, double* r, double* v, do
                 a[i]=0;
             }
         }
-        //Calculate new accelerations and velocities
+        }
+        //Calculate new accelerations, velocities and system energy
+        *energy=0;
+        #pragma omp parallel for private(a_next,i)
+        {
         for(int i=0;i<num_shells;i+=1){
+            *energy += (-G*pow(mass[i],2))/(2*r[i]);
+            *energy += .5*mass[i]*pow(v[i],2);
             if(r[i]>1){
                 a_next = (-G*mass[i])/(2*pow(r[i],2));
                 for(int j=0;j<num_shells;j+=1){
                     if(i!=j & r[j]<r[i]){
                         a_next += (-G*mass[j])/(pow(r[i],2));
+                        *energy += (-G*mass[j]*mass[i])/r[i];
                     }
                 }
                 v[i] = v[i]+0.5*(a[i]+a_next)*dt;
                 a[i] = a_next;
             }
         }
-        //Calculate new system energy
-        *energy=0;
-        for(int i=0;i<num_shells;i+=1){
-            *energy += (-G*pow(mass[i],2))/(2*r[i]);
-            *energy += .5*mass[i]*pow(v[i],2);
-            for(int j=0;j<num_shells;j+=1){
-                if(j!=i & r[j]<r[i]){
-                    *energy += (-G*mass[j]*mass[i])/r[i];
-                }
-            }
         }
         //Advance to next time step
         t+=dt;
