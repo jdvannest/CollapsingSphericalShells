5f990564f6957c53375dc0ae46ce454cd32440c9
jdvannest <jordanvannest@gmail.com>
2021-05-11 00:08:06 -0500

    Energy now calculated at the end of timestep. Fully collapsed shells no longer contribute.

--- evolve.cpp
+++ evolve.cpp
@@ -47,8 +47,6 @@ void evolve(double time, double dt, const double* mass, double* r, double* v, do
         //Calculate new accelerations, velocities, and system energy
         *energy=0;
         for(int i=0;i<num_shells;i+=1){
-            *energy += (-G*pow(mass[i],2))/(2*r[i]);
-            *energy += .5*mass[i]*pow(v[i],2);
             if(r[i]>1){
                 a_next = (-G*mass[i])/(2*pow(r[i],2));
                 for(int j=0;j<num_shells;j+=1){
@@ -57,6 +55,8 @@ void evolve(double time, double dt, const double* mass, double* r, double* v, do
                         *energy += (-G*mass[j]*mass[i])/r[i];
                     }
                 }
+                *energy += (-G*pow(mass[i],2))/(2*r[i]);
+                *energy += .5*mass[i]*pow(v[i],2);
                 v[i] = v[i]+0.5*(a[i]+a_next)*dt;
                 a[i] = a_next;
             }
